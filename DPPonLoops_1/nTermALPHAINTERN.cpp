cfa27dde7dc2d15d7fdffd6623a825958344fa3e
DearPratipal <kumarpratipal123@gmnail.com>
2025-11-28 21:53:01 +0530

    Commit - Message

---
+++ DPPonLoops_1/nTermALPHAINTERN.cpp
@@ -0,0 +1,13 @@
+// Take input integer n and print “Alpha Intern” n times.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Alpha Intern" << endl;
+    }
+    return 0;
+}
