633573b756834ebbc2068e37a98d0e4132282f13
DearPratipal <kumarpratipal123@gmnail.com>
2025-11-28 21:42:02 +0530

    sum of two integer question

---
+++ DppOnBasic/sumOfTwoInteger.cpp
@@ -0,0 +1,12 @@
+// Take a two integer inputs from the user and print their sum.
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    int sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is " << sum << endl;
+    return 0;
+}
